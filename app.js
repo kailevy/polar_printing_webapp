47e222c23b8cdbcf0e283ce84bfd1abc9f05d1b7
kailevy <kai0levy@gmail.com>
2016-01-09 22:24:24 -0800

    Add initial files

---
+++ app.js
@@ -0,0 +1,23 @@
+// npm modules
+var express = require('express');
+var path = require('path');
+var winston = require('winston');
+var bodyParser = require('body-parser');
+// routes
+var routes = require('./routes/routes.js');
+// app init
+var app = express();
+
+// port set up
+var PORT = process.env.PORT || 4000;
+
+// middleware?
+global.logger = winston;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+routes(app);
+
+app.listen(PORT);
+logger.info('Listening on Port', PORT);
