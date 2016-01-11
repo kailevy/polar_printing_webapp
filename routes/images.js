b918830bfac57046798c795153499d2b4a303fb1
kailevy <kai0levy@gmail.com>
2016-01-10 23:12:03 -0800

    Reverse image order

--- routes/images.js
+++ routes/images.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 function images(req, res) {
   logger.info('GET images');
-  var files = fs.readdirSync(path.resolve('public/uploads'));
+  var files = fs.readdirSync(path.resolve('public/uploads')).slice().reverse();
   logger.info(files);
   res.status(200).send(files);
 }
